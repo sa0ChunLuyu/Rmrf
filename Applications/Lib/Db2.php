0c24e4c8d7f28ae5b805905b34511a61c0873ac2
sa0ChunLuyu <sa0Chun_luyu@aliyun.com>
2024-06-16 21:38:27 +0800

    项目初始化

---
+++ Applications/Lib/Db2.php
@@ -0,0 +1,42 @@
+<?php
+
+class Db2
+{
+  public static function i($db, $table, $array)
+  {
+    $insertArr = array();
+    $sql = "insert into `{$table}`(";
+    $sql1 = 'values(';
+    foreach ($array as $key => $value) {
+      $sql .= "`{$key}`,";
+      $sql1 .= "?,";
+      $insertArr[] = $value;
+    }
+    $sql = trim($sql, ',');
+    $sql1 = trim($sql1, ',');
+    $sql .= ")" . $sql1 . ")";
+    $db->querysql($sql, $insertArr);
+    return $db->getinsertid();
+  }
+
+  public static function u($db, $table, $array, $where, $where_a = [])
+  {
+    $updateArr = array();
+    $sql = "update `{$table}` set ";
+    foreach ($array as $key => $value) {
+      $sql .= "`{$key}`=?,";
+      $updateArr[] = $value;
+    }
+    $sql = trim($sql, ',');
+    $where = ' ' . $where;
+    $sql .= $where;
+    $updateArr = array_merge($updateArr, $where_a);
+    return $db->querysql($sql, $updateArr);
+  }
+
+  public static function d($db, $table, $where, $where_a = [])
+  {
+    $sql = "delete from `{$table}` " . $where;
+    $db->querysql($sql, $where_a);
+  }
+}
