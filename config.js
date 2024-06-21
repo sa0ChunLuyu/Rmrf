6d94a783511d0be3c89895d405bd5c8b99bc6934
sa0ChunLuyu <sa0Chun_luyu@aliyun.com>
2024-06-21 23:24:50 +0800

    更新 1. 修改接口缓存逻辑 2. 增加 url 方法

--- config.js
+++ config.js
@@ -3,18 +3,11 @@ const config = {
 	base_assets_url: 'http://rmrf.pi.sa0.online',
 }
 uni.$config = JSON.parse(JSON.stringify(config))
-const config_str_key = "CONFIG_STR"
-let config_str = uni.getStorageSync(config_str_key)
-if (!config_str) {
-	config_str = JSON.stringify(config)
-	uni.setStorageSync(config_str_key, config_str)
-}
-const config_data = JSON.parse(config_str)
 export default {
 	title: '入魔入佛',
 	success_code: 200,
 	loading: false,
 	loading_text: '网络请求中，请稍等',
 	error_message: '网络请求发生错误',
-	config: config_data
+	config: config
 }
